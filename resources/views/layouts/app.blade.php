632dbf73c755029c9777c21a83e221542077c5e1
Ahsiiali07 <ahsan8642064@gmail.com>
2024-03-06 13:37:20 +0500

    main

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <meta content="Themesbrand" name="author"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ URL::asset('assets/images/favicon.ico') }}">
+    <link rel="shortcut icon" href="{{ URL::asset('assets/img/logo.svg') }}">
 
     @include('layouts.header.head-css')
     <style>
